5df1681e33107666c50d24d9910523dd1f6e0ac3
Dave Babbitt <dave.babbitt@gmail.com>
2020-11-16 07:06:24 -0500

    Changed to single quotes and other administrivia

--- ps1/create_covid19_conda_environment.ps1
+++ ps1/create_covid19_conda_environment.ps1
@@ -1,6 +1,10 @@
 
+# cd C:\Users\dev\Documents\repositories\covid19\ps1
+# clear
+# .\create_covid19_conda_environment.ps1
+
 # Set up global variables
 $DisplayName = "COVID-19"
-$RepositoryPath = "notebooks/covid19"
+$RepositoryPath = "covid19"
 $EnvironmentName = "covid19"
 ."C:\Users\dev\Documents\repositories\notebooks\ps1\create_conda_environment.ps1"
--- ps1/run_jupyter_lab.ps1
+++ ps1/run_jupyter_lab.ps1
@@ -4,6 +4,21 @@ $DisplayName = "COVID-19"
 $RepositoryPath = "notebooks/covid19"
 $EnvironmentName = "covid19"
 ."C:\Users\dev\Documents\repositories\notebooks\ps1\create_conda_environment.ps1"
+
+# JupyterLab sessions always reside in a workspace. Workspaces contain the state of
+# JupyterLab: the files that are currently open, the layout of the application areas
+# and tabs, etc. When the page is refreshed, the workspace is restored.
+
+# The default workspace does not have a name and resides at the primary /lab URL:
+# http(s)://<server:port>/<lab-location>/lab
+
+# Restart jupyter lab
+Write-Host ""
+Write-Host "---------------------------------------------------------------------------------" -ForegroundColor Green
+Write-Host "                             Restarting Jupyter Lab" -ForegroundColor Green
+Write-Host "---------------------------------------------------------------------------------" -ForegroundColor Green
+jupyter lab
+
 Write-Host ""
 Write-Host "---------------------------------------------------------------------------------" -ForegroundColor Green
 Write-Host "                          Getting the Jupyter Lab token" -ForegroundColor Green
@@ -13,8 +28,11 @@ $ListResults = (jupyter notebook list) | Out-String
 $TokenString = $TokenRegex.Match($ListResults).Groups[1].Value
 Write-Host $ListResults
 
-# Restart jupyter lab
-jupyter lab
-
 # Open the webpage in Chrome
-Start-Process "chrome.exe" "http://localhost:8888/lab?clone=${EnvironmentName}&token=${TokenString}"
+Write-Host ""
+Write-Host "---------------------------------------------------------------------------------" -ForegroundColor Green
+Write-Host "                          Opening the workspace in Chrome" -ForegroundColor Green
+Write-Host "---------------------------------------------------------------------------------" -ForegroundColor Green
+# All other workspaces have a name that is part of the URL:
+# http(s)://<server:port>/<lab-location>/lab/workspaces/foo
+Start-Process "chrome.exe" "http://localhost:8888/lab/workspaces/${EnvironmentName}&token=${TokenString}"
